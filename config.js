6235d01241668db0b5ddfbd37bb16696aabe5157
Acutus AI <social@acutusai.com>
2024-11-28 05:19:51 +0000

    changes

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ const sequelize = new Sequelize('u411184336_acutusaidb', 'u411184336_acutusaidb'
   port: 3306,        
   dialect: 'mysql',
   pool: {
-    max: 150,           // Adjust based on traffic
+    max: 150,          
     min: 10,
-    acquire: 30000,     // Wait time to acquire a connection
-    idle: 10000,        // Time before closing idle connections
-    evict: 15000        // Check idle connections every 15 seconds
+    acquire: 30000,     
+    idle: 10000,        
+    evict: 15000    
   },
 });
 
