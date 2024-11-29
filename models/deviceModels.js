43dcf8942c96ddb6184c4bb040c1d4e3455e88f2
Acutus AI <social@acutusai.com>
2024-11-29 13:49:18 +0530

    Update deviceModels.js

--- models/deviceModels.js
+++ models/deviceModels.js
@@ -56,45 +56,6 @@ const DeviceInfo = sequelize.define(
     JavaEnabled: {
       type: DataTypes.BOOLEAN,
     },
-    Platform: {
-      type: DataTypes.STRING,
-    },
-    Referrer: {
-      type: DataTypes.STRING,
-      validate: {
-        isUrl: true,  // Validate that the referrer is a URL
-      },
-    },
-    NetworkType: {
-      type: DataTypes.STRING,
-    },
-    DeviceMemory: {
-      type: DataTypes.FLOAT,  // Device memory in GB
-    },
-    HardwareConcurrency: {
-      type: DataTypes.INTEGER,  // Number of logical CPU cores
-    },
-    BatteryLevel: {
-      type: DataTypes.FLOAT,  // Battery level as a percentage
-    },
-    GpuVendor: {
-      type: DataTypes.STRING,
-    },
-    GpuRenderer: {
-      type: DataTypes.STRING,
-    },
-    BrowserPlugins: {
-      type: DataTypes.JSON,  // Array of browser plugin names
-    },
-    CanvasFingerprint: {
-      type: DataTypes.STRING,  // Unique canvas fingerprint
-    },
-    WebGLFingerprint: {
-      type: DataTypes.STRING,  // Unique WebGL fingerprint
-    },
-    AudioFingerprint: {
-      type: DataTypes.STRING,  // Audio fingerprint
-    },
     Timestamp: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
