214aa60ea82d9419b5645188980394a5b4c897ce
Acutus AI <social@acutusai.com>
2025-01-06 16:25:10 +0530

    Update Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -1,34 +1,87 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config');  
+const sequelize = require('./config'); // Import the correct sequelize instance
 
-const SurveyStatus = sequelize.define('SurveyStatus', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    
-  },
-  panelistID : {
-    type: DataTypes.STRING,
-  },
-  points: {
-    type: DataTypes.INTEGER,
-    
-    defaultValue: 0,
-  },
-  status: {
-    type: DataTypes.STRING,
-    
-    validate: {
-      isIn: [['terminate', 'complete', 'quotafull', "quality"]], // Allowed statuses
+const UserProfile = sequelize.define('UserProfile', {
+    id  : {
+        type : DataTypes.INTEGER,
+        primaryKey : true,
+        autoIncrement : true
+
     },
-  },
+    point : {
+        type : DataTypes.INTEGER
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            isNumeric: true,
+        },
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    state: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    country: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    postalCode: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            isNumeric: true,
+        },
+    },
+    dateOfBirth: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+    },
+    gender: {
+        type: DataTypes.ENUM('Male', 'Female', 'Other'),
+        allowNull: true,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
 }, {
-  tableName: 'survey_status',
-  timestamps: true, 
+    tableName: 'user_profiles',
+    timestamps: true,
 });
 
-module.exports = SurveyStatus;
+module.exports = UserProfile; // Export the model
