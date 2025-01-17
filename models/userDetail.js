9a81e531c7d4d97fdce0322856e851a3b657f277
Acutus AI <social@acutusai.com>
2025-01-17 15:50:34 +0530

    Update userDetail.js

--- models/userDetail.js
+++ models/userDetail.js
@@ -1,58 +1,54 @@
-const { Model, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config'); // Import sequelize instance from config.js
 
-module.exports = (sequelize) => {
-  class UserDetail extends Model {} // Changed model name to UserDetail
-
-  UserDetail.init(
-    {
-      firebaseInfo: {
-        type: DataTypes.JSONB, // Storing the firebaseInfo object as JSON
-        allowNull: true,
-      },
-      token: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      timestamp: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      identities: {
-        type: DataTypes.JSONB, // Storing the identities array as JSON
-        allowNull: false,
-      },
-      idToken: {
-        type: DataTypes.TEXT, // For longer JWT tokens
-        allowNull: false,
-      },
-      network: {
-        type: DataTypes.JSONB, // Storing network object as JSON
-        allowNull: true,
-      },
-      deviceInfo: {
-        type: DataTypes.JSONB, // Storing deviceInfo object as JSON
-        allowNull: true,
-      },
-      sessionInfo: {
-        type: DataTypes.JSONB, // Storing sessionInfo object as JSON
-        allowNull: true,
-      },
+const UserDetail = sequelize.define(
+  'UserDetail',
+  {
+    firebaseInfo: {
+      type: DataTypes.JSONB,
+      allowNull: true,
     },
-    {
-      sequelize,
-      modelName: 'UserDetail', // Updated model name
-      tableName: 'user_details', // Custom table name
-      timestamps: false, // Disable Sequelize's automatic timestamps
-    }
-  );
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    identities: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+    idToken: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    network: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+    },
+    deviceInfo: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+    },
+    sessionInfo: {
+      type: DataTypes.JSONB,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'user_details',
+    timestamps: false,
+  }
+);
 
-  return UserDetail;
-};
+module.exports = UserDetail;
